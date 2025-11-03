9a29f4f0f778901d58f53e5111a181938ec8931c
Nour <nour.diab09@lau.edu>
2025-11-03 17:44:24 +0200

    Fixed merge conflicts

--- plateful/src/App.jsx
+++ plateful/src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Splash from "./Splash/splash.jsx"; // splash screen
+import Splash from "./Splash/splash.jsx";
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import RecipeDetails from "./pages/RecipeDetails"; // edited by Noura
 import AddEditRecipe from "./pages/AddEditRecipe"; // edited by Noura
-import FavoriteRecipes from "./pages/FavoriteRecipes"; // edited by Noura
 import PrivacyPolicy from "./Auth/PrivacyPolicy.jsx";
 import SignUp from "./Auth/SignUp";
 import LogIn from "./Auth/LogIn.jsx";
@@ -22,7 +21,6 @@ export default function App() {
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} /> {/* edited by Noura */}
         <Route path="/add" element={<AddEditRecipe />} /> {/* edited by Noura */}
-        <Route path="/favorites" element={<FavoriteRecipes />} /> {/* edited by Noura */}
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/log-in" element={<LogIn />} />
